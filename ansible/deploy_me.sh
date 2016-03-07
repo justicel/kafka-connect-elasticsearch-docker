0d446a0dc58a7e509129adcb6e499009ccf72e00
matteo luzzi <matteo@vimond.com>
2016-03-07 15:57:17 +0100

    Added env variables to the ansible deploy script and removed unused operation when launched in distributed mode

--- ansible/deploy_me.sh
+++ ansible/deploy_me.sh
@@ -2,7 +2,6 @@
 
 COMPONENT="vimond-kafka-connect"
 
-
 tar -cvf "temp.tar" -C ansible/src/user/dist .
 curl -i -u"$ARTIFACTORY_USER:$ARTIFACTORY_PASSWORD" -T temp.tar \
     "$ARTIFACTORY_CONTEXTURL/ansible/$COMPONENT/$2/$COMPONENT-$2.tar;git_sha=$1;version=$2;build.name=$3"
--- start-kafka-connect.sh
+++ start-kafka-connect.sh
@@ -47,13 +47,6 @@ elif [[ ${KAFKA_CONNECT_MODE} == 'distributed' ]]; then
     $KAFKA_HOME/bin/kafka-topics.sh --create --zookeeper $ZOOKEEPER_ADDRESS:2181 --replication-factor 1 --partition 1 --topic ${CONFIG_STORAGE_TOPIC}
     $KAFKA_HOME/bin/kafka-topics.sh --create --zookeeper $ZOOKEEPER_ADDRESS:2181 --replication-factor 1 --partition 20 --topic ${OFFSET_STORAGE_TOPIC}
 
-    cat /opt/kafka_${SCALA_VERSION}-${KAFKA_VERSION}/config/${CONFIG_FILE}.template | sed \
-    -e "s|{{CONFIG_STORAGE_TOPIC}}|${CONFIG_STORAGE_TOPIC}|g" \
-    -e "s|{{OFFSET_STORAGE_TOPIC}}|${OFFSET_STORAGE_TOPIC}|g" \
-    -e "s|{{CONNECT_ADVERTISED_HOSTNAME}}|${CONNECT_ADVERTISED_HOSTNAME:-$IP}|g" \
-    -e "s|{{KAFKA_ADDRESS}}|${KAFKA_ADDRESS:-kafka}|g" \
-    > /opt/kafka_${SCALA_VERSION}-${KAFKA_VERSION}/config/${CONFIG_FILE}-new.properties
-
     exec /opt/kafka_${SCALA_VERSION}-${KAFKA_VERSION}/bin/${BIN_EXEC}.sh /opt/kafka_${SCALA_VERSION}-${KAFKA_VERSION}/config/${CONFIG_FILE}-new.properties
 
 else
